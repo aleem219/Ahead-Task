e4e4f49cecce397bcf18eb38902c3722bd6bb1cc
Your GitHub aleem219 <Your GitHub qazialeem21@gmail.com>
2026-03-03 21:39:44 +0530

    Initial Commit

---
+++ AheadTask/AheadTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AheadTaskApp.swift
+//  AheadTask
+//
+//  Created by Abdul Aleem on 03/03/26.
+//
+
+import SwiftUI
+
+@main
+struct AheadTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
